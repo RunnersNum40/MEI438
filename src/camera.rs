a49c170a4433ceba1e5a189867ea4360f884bbe4
Ted <ted.pinkerton@mail.utoronto.ca>
2025-04-06 02:12:42 -0400

    Seperate camera logic

---
+++ src/camera.rs
@@ -0,0 +1,58 @@
+use anyhow::Result;
+use esp_camera_rs::Camera;
+use esp_idf_hal::gpio;
+use esp_idf_sys::camera;
+
+pub struct Frame {
+    pub data: Vec<u8>,
+    pub width: usize,
+    pub height: usize,
+}
+
+pub struct CameraWrapper {
+    camera: Camera<'static>,
+    width: usize,
+    height: usize,
+}
+
+impl CameraWrapper {
+    pub fn new(pins: gpio::Pins) -> Result<Self> {
+        let cam = Camera::new(
+            pins.gpio32,
+            pins.gpio0,
+            pins.gpio5,
+            pins.gpio18,
+            pins.gpio19,
+            pins.gpio21,
+            pins.gpio36,
+            pins.gpio39,
+            pins.gpio34,
+            pins.gpio35,
+            pins.gpio25,
+            pins.gpio23,
+            pins.gpio22,
+            pins.gpio26,
+            pins.gpio27,
+            camera::pixformat_t_PIXFORMAT_GRAYSCALE,
+            camera::framesize_t_FRAMESIZE_QQVGA,
+        )?;
+        Ok(CameraWrapper {
+            camera: cam,
+            width: 160,
+            height: 120,
+        })
+    }
+
+    pub fn capture_frame(&mut self) -> Result<Frame> {
+        let fb = self
+            .camera
+            .get_framebuffer()
+            .ok_or_else(|| anyhow::anyhow!("Failed to get framebuffer"))?;
+
+        Ok(Frame {
+            data: fb.data().to_vec(),
+            width: fb.width(),
+            height: fb.height(),
+        })
+    }
+}
